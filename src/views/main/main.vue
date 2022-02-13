c5051ce7f8b4ad302e17bffb50fdd2edceedcb62
FangDizhong <dezhongfeifan@gmail.com>
2022-02-13 17:59:18 +0900

    fix(pagequeryinfo): make pageQueryInfo consistent

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <el-container class="main-content">
       <!-- 菜单 -->
-      <el-aside :width="isCollapse ? '60px' : '210px'">
+      <el-aside :width="isCollapse ? '60px' : '180px'">
         <nav-menu :collapse="isCollapse" />
       </el-aside>
 
